281672e14c419ebd6bba4b661dadeaf338e568d1
Do Chi Hung <dochihung492002@gmail.com>
2022-11-23 16:16:28 +0700

    update

--- Shop.Web/app/components/header/header.controller.js
+++ Shop.Web/app/components/header/header.controller.js
@@ -1,10 +1,33 @@
 ﻿// Register `phoneList` component, along with its associated controller and template
 
 angular.
-    module('shop.header').
+    module('shop.header', ['shop.common']).
     component('headerView', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
         templateUrl: "/app/components/header/header.view.html",
-        controller: function HeaderController() {
-            
-        }
+        controller: ['apiService', function HeaderController(apiService) {
+            var self = this;
+
+            this.productCategories = [];
+
+
+
+            function getListProductCategory() {
+
+                apiService.get(
+                    'https://localhost:44353/api/productcategory/getallparents',
+                    null,
+                    function (result) {
+                        self.productCategories = result.data;
+                        console.log(self.productCategories)
+                    },
+                    function (error) {
+                        console.log('Lấy danh mục sản phẩm không thành công');
+                    }
+                );
+            }
+
+            getListProductCategory();
+
+
+        }]
     });
--- Shop.Web/app/shared/services/authData.js
+++ Shop.Web/app/shared/services/authData.js
@@ -1,1 +1,19 @@
-﻿
+﻿(function (app) {
+    'use strict'
+
+    app.factory('authData', [
+        function () {
+            var authDataFactory = {};
+
+            var authentication = {
+                IsAuthenticated: false,
+                userName: ""
+            };
+
+            authDataFactory.authenticationData = authentication;
+
+            return authDataFactory;
+        }
+    ])
+
+})(angular.module('shop.common'));
