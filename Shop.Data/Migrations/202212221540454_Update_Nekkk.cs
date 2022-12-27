616b6ba999d3d311145821a601716e8278c8f5e7
Đỗ Chí Hùng <dochihung492002@gmail.com>
2022-12-27 12:33:50 +0700

    update

---
+++ Shop.Data/Migrations/202212221540454_Update_Nekkk.cs
@@ -0,0 +1,18 @@
+﻿namespace Shop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update_Nekkk : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Orders", "PaymentMethod", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Orders", "PaymentMethod", c => c.String());
+        }
+    }
+}
---
+++ Shop.Data/Migrations/202212230317121_Update_Identity.cs
@@ -0,0 +1,22 @@
+﻿namespace Shop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update_Identity : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.AspNetUserClaims", newName: "ApplicationUserClaims");
+            RenameTable(name: "dbo.AspNetUserLogins", newName: "ApplicationUserLogins");
+            RenameTable(name: "dbo.AspNetRoles", newName: "ApplicationRoles");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.ApplicationRoles", newName: "AspNetRoles");
+            RenameTable(name: "dbo.ApplicationUserLogins", newName: "AspNetUserLogins");
+            RenameTable(name: "dbo.ApplicationUserClaims", newName: "AspNetUserClaims");
+        }
+    }
+}
